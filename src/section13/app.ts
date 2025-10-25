2860137c56512f2fab2273f0f4f5bdb2efaf63ad
jgvkmea <kenseito25@gmail.com>
2025-10-25 22:02:43 +0900

    add lecture 175

--- src/section13/app.ts
+++ src/section13/app.ts
@@ -1,12 +1,37 @@
+import axios from "axios";
+
 const form13 = document.querySelector("form")!;
 const addressEl13 = document.getElementById("address")! as HTMLInputElement;
 
+type googleGeoResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
 const GOOGLE_MAP_API_KEY = process.env.GOOGLE_MAP_API_KEY;
-console.log(GOOGLE_MAP_API_KEY);
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
-  // const address13 = addressEl13.value;
+  const enteredAddress13 = addressEl13.value;
+
+  axios
+    .get<googleGeoResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress13
+      )}&key=${GOOGLE_MAP_API_KEY}`
+    )
+    .then((res) => {
+      if (res.data.status != "OK") {
+        throw new Error("結果が0件でした");
+      }
+      const coor = res.data.results[0].geometry.location;
+      console.log(res.data.results);
+      console.log(coor);
+    })
+    .catch((error) => {
+      alert(error.message);
+      console.log(error);
+    });
 }
 
 form13.addEventListener("submit", searchAddressHandler);
